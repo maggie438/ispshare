dccbe9ac5eb76a90ec9dd6f9de9463734f6515a4
Meghana Pula <saimeghana.pula@student.allenisd.org>
2022-04-28 18:29:17 -0500

    hi

--- Sources/ScenesShell/Background.swift
+++ Sources/ScenesShell/Background.swift
@@ -11,5 +11,6 @@ class Background : RenderableEntity {
       init() {
           // Using a meaningful name can be helpful for debugging
           super.init(name:"Background")
+          System.out.println("hi")
       }
 }
